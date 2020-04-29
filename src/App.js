4c25ff18bb674254957b303e09d68de3d8d796fe
Maksym Skoryk <mskory@softserveinc.com>
2020-04-29 15:47:59 +0300

    small change on a feature branch

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          I was changed on a feature branch!
+          I was changed on a feature branch again!
         </p>
         <a
           className="App-link"
